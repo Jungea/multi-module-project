1c02c96c2560118e0deff82be8f1a5501f8e442d
Jungea <jungea2241@gmail.com>
2023-06-20 23:56:09 +0900

    멀티 모듈 생성, 실행 설정

--- module-api/src/main/java/com/example/ModuleApiApplication.java
+++ module-api/src/main/java/com/example/ModuleApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.multimoduleproject;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiModuleProjectApplication {
+public class ModuleApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultiModuleProjectApplication.class, args);
+        SpringApplication.run(ModuleApiApplication.class, args);
     }
 
 }
